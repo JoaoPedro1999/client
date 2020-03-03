8539191082f334bb2c05d07c8c3c9b6f7c4a33cc
João Pedro <joaopedrobeckland@gmail.com>
2020-03-03 20:25:41 -0300

    feat: start Dashboard development

--- src/routes/Route.tsx
+++ src/routes/Route.tsx
@@ -1,19 +1,17 @@
 import React from 'react';
 import { Route, Redirect, RouteProps } from 'react-router-dom';
-// import { useSelector } from 'react-redux';
-// import { ApplicationState } from 'store';
+import { useSelector } from 'react-redux';
+import { ApplicationState } from '../store';
 
 import AuthLayout from '../pages/_layouts/auth';
-// import DefaultLayout from '../pages/_layouts/default';
+import DefaultLayout from '../pages/_layouts/default';
 
 interface Props extends RouteProps {
   isPrivate?: boolean;
 }
 
 export default function RouteWrapper({ component, isPrivate, ...rest }: Props) {
-  // const signed = useSelector((state: ApplicationState) => state.auth.signed);
-
-  const signed = false;
+  const signed = useSelector((state: ApplicationState) => state.auth.signed);
 
   if (!signed && isPrivate) {
     return <Redirect to="/" />;
@@ -23,8 +21,7 @@ export default function RouteWrapper({ component, isPrivate, ...rest }: Props) {
     return <Redirect to="/dashboard" />;
   }
 
-  // const Layout = signed ? DefaultLayout : AuthLayout;
-  const Layout = AuthLayout;
+  const Layout = signed ? DefaultLayout : AuthLayout;
 
   const Component: any = component;
   return (
@@ -38,34 +35,3 @@ export default function RouteWrapper({ component, isPrivate, ...rest }: Props) {
     />
   );
 }
-
-// export default RouterWrapper;
-
-// export default function RouteWrapper({
-//   component: Component,
-//   isPrivate,
-//   ...rest
-// }) {
-//   const { signed } = store.getState().auth;
-
-//   if (!signed && isPrivate) {
-//     return <Redirect to="/" />;
-//   }
-
-//   if (signed && !isPrivate) {
-//     return <Redirect to="/dashboard" />;
-//   }
-
-//   const Layout = signed ? DefaultLayout : AuthLayout;
-
-//   return (
-//     <Route
-//       {...rest}
-//       render={props => (
-//         <Layout>
-//           <Component {...props} />
-//         </Layout>
-//       )}
-//     />
-//   );
-// }
